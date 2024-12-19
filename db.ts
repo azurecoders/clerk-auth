4a88370bf305f619b985ce0e93fd7dd6c8d574c8
Muzammil Ahmed <166981810+azurecoders@users.noreply.github.com>
2024-12-19 14:42:29 +0500

    UPDATED DB FILE

--- db.ts
+++ db.ts
@@ -17,16 +17,21 @@ if (!cached) {
 }
 
 export const connect = async () => {
-  if (cached.conn) return cached.conn;
+  try {
+    if (cached.conn) return cached.conn;
 
-  cached.promise =
-    cached.promise ||
-    mongoose.connect(MONGODB_URL, {
-      dbName: "clerk-auth",
-      bufferCommands: false,
-      connectTimeoutMS: 30000,
-    });
+    cached.promise =
+      cached.promise ||
+      mongoose.connect(MONGODB_URL, {
+        dbName: "clerk-auth",
+        bufferCommands: false,
+        connectTimeoutMS: 30000,
+      });
 
-  cached.conn = await cached.promise;
-  return cached.conn;
+    cached.conn = await cached.promise;
+    return cached.conn;
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    throw error;
+  }
 };
